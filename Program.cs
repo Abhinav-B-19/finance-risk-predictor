71c15e0a652c6419d20a97602faf04df639b07e2
Abhinav-B-19 <shreeabhi2002@gmail.com>
2026-05-07 03:17:16 +0530

    Prepare backend API for production deployment

--- Program.cs
+++ Program.cs
@@ -9,8 +9,19 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<PredictionService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
+app.UseCors("AllowAll");
 
 // Middleware
 if (app.Environment.IsDevelopment())
